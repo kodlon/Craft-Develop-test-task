7405bef4ae688fd5d5aa020decb770900f8ff291
Ihor Chuchman <45404949+kodlon@users.noreply.github.com>
2021-08-10 18:08:03 +0300

    Create PlatformSpawn.cs

---
+++ CD Games Project/Assets/Scripts/Spawn/PlatformSpawn.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Spawn
+{
+    public class PlatformSpawn : MonoBehaviour
+    {
+        [SerializeField] private GameObject[] platformObject;
+        private int _chosenObject;
+
+        private Coroutine _spawnerActive;
+
+
+        private void Start()
+        {
+            _spawnerActive = StartCoroutine(Spawn());
+            _chosenObject = Random.Range(0, platformObject.Length);
+        }
+
+        private IEnumerator Spawn()
+        {
+            while (true)
+            {
+                GameObject platform = Instantiate(platformObject[_chosenObject], new Vector3(50, 0, 0),
+                    transform.rotation);
+                yield break;
+            }
+        }
+
+        public void DeactiveSpawner()
+        {
+            StopCoroutine(_spawnerActive);
+        }
+    }
+}
