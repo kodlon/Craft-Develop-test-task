192d7f5486364ec59571faf45ce4b0bb49d10620
Ihor Chuchman <45404949+kodlon@users.noreply.github.com>
2021-08-10 22:27:48 +0300

    Add object destroy

--- CD Games Project/Assets/Scripts/Spawn/PlatformObject.cs
+++ CD Games Project/Assets/Scripts/Spawn/PlatformObject.cs
@@ -1,9 +1,15 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 namespace Spawn
 {
     public class PlatformObject : MonoBehaviour
     {
+        private void Start()
+        {
+            Destroy(this, 60f);
+        }
+
         private void Update()
         {
             transform.Translate(Vector3.left * (Time.deltaTime * 4f));
