ddec9a73bbdc5c74476de706a5f9e4630a2e159e
Ihor Chuchman <45404949+kodlon@users.noreply.github.com>
2021-08-10 18:01:21 +0300

    Create PlatformObject.cs

---
+++ CD Games Project/Assets/Scripts/Spawn/PlatformObject.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace Spawn
+{
+    public class PlatformObject : MonoBehaviour
+    {
+        private void Update()
+        {
+            transform.Translate(Vector3.left * (Time.deltaTime * 4f));
+        }
+    }
+}
