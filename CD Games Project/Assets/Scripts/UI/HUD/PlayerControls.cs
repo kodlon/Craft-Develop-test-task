77ca4dae2cbb745402a92835898550cdf6b5d8d6
Ihor Chuchman <45404949+kodlon@users.noreply.github.com>
2021-08-11 11:56:26 +0300

    Added moving and visibility of slider, test

---
+++ CD Games Project/Assets/Scripts/UI/HUD/PlayerControls.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using UI.MainMenu;
+using UnityEngine;
+
+namespace UI.HUD
+{
+    public class PlayerControls : MonoBehaviour
+    {
+        [SerializeField] private GameObject slider;
+        private static GameObject s_slider;
+
+        private void Start()
+        {
+            s_slider = slider;
+        }
+
+        public static void VisibilityOfSlider(Vector3 position, bool isVisible = false)
+        {
+            if (StartedGame.IsGameStarted)
+            {
+                s_slider.SetActive(isVisible);
+                s_slider.transform.position = position;
+            }
+        }
+    }
+}
