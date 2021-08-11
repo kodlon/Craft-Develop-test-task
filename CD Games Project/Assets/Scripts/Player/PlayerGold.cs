4c1059dfd1451752d9aac051a6658a92418fd8fe
Ihor Chuchman <45404949+kodlon@users.noreply.github.com>
2021-08-11 11:54:34 +0300

    Added PlayerGold.cs for equip gold on level

---
+++ CD Games Project/Assets/Scripts/Player/PlayerGold.cs
@@ -0,0 +1,39 @@
+﻿using Other;
+using UnityEngine;
+
+namespace Player
+{
+    public class PlayerGold : MonoBehaviour
+    {
+        private Ray _ray;
+
+        private int _gold = 0;
+
+        public void GoldCheck()
+        {
+            RayInitialize();
+
+            if (Physics.Raycast(_ray, out RaycastHit hit) & hit.collider != null & hit.distance < 1f)
+            {
+                Gold gold = hit.collider.GetComponent<Gold>();
+                _gold += 2;
+                Debug.Log(_gold);
+                Destroy(gold.gameObject);
+            }
+        }
+
+        private void RayInitialize()
+        {
+            _ray = new Ray(transform.position + new Vector3(1, 1, 0), Vector3.right);
+        }
+
+        private void OnDrawGizmos()
+        {
+            RayInitialize();
+
+            Gizmos.color = Color.red;
+
+            Gizmos.DrawRay(_ray.origin, _ray.direction);
+        }
+    }
+}
