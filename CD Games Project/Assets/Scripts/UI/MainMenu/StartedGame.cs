0ccf13307d04adf6ad51ef883f8e96685319abc3
Ihor Chuchman <45404949+kodlon@users.noreply.github.com>
2021-08-11 11:57:37 +0300

    Created StartedGame.cs and added on Start button press method

---
+++ CD Games Project/Assets/Scripts/UI/MainMenu/StartedGame.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace UI.MainMenu
+{
+    public class StartedGame : MonoBehaviour
+    {
+        public static bool IsGameStarted { get; set; }
+
+        public void GameStarted()
+        {
+            IsGameStarted = true;
+            
+            gameObject.SetActive(false);
+        }
+    }
+}
