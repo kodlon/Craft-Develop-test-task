84cf609dc99ed793b4323c8683619196aeae15c1
Ihor Chuchman <45404949+kodlon@users.noreply.github.com>
2021-08-12 05:21:40 +0300

    Added BackgroundMove.cs

---
+++ CD Games Project/Assets/Scripts/Other/BackgroundMove.cs
@@ -0,0 +1,16 @@
+﻿using UI.MainMenu;
+using UnityEngine;
+
+namespace Other
+{
+    public class BackgroundMove : MonoBehaviour
+    {
+        private float _speed = 10f;
+
+        private void Update()
+        {
+            if (StartedGame.IsGameStarted)
+                transform.Translate(Vector3.right * (_speed * Time.deltaTime));
+        }
+    }
+}
